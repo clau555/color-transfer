33ffec84aa12d7b452195a507e0f9891b3d21e3e
Lucas <lucas.chardonnet@etu.univ-smb.fr>
2022-05-10 00:11:43 +0100

    Update algorithm to vector search instead of random vector draws

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Color transfer by one-dimensional optimal transport
+# Color transfer using one-dimensional optimal transport
 
 A color transfer consists of giving the colors of a source image to a target image.<br>
-Here, a pixel of the source can be used only one time.<br>
+Here, a pixel of the source can be used only one time, and the two images must be of the same area.<br>
 The operation then boils down to rearranging the pixels of the source image to make it resemble the target image as 
 closely as possible.
 
@@ -11,16 +11,50 @@ closely as possible.
 - [pillow](https://pypi.org/project/Pillow/) ```pip install pillow```
 - [tqmd](https://pypi.org/project/tqdm/) ```pip install tqmd```
 
-## Usage example
+## Use
 
-```python color_transfer.py space.jpg sky.jpg -sa output.jpg -sh```<br>
-The colors of sky.jpg will be transferred to space.jpg, the output image will be stored inside output.jpg,
+```
+positional arguments:
+  target_image          Targeted image.
+  source_image          Source image.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -sa SAVE, --save SAVE
+                        Output file to save.
+  -sh, --show           Preview the output at the end.
+```
+
+## Example
+
+```
+python color_transfer.py space.jpg sky.jpg -sa output.jpg -sh
+```
+The colors of `sky.jpg` will be transferred to `space.jpg`, the output image will be stored inside `output.jpg`,
 and a preview of the output will be show at the end of the execution.
 
-Inputs
+### Inputs
 
-<img src="space.jpg" width="320" alt="target_img"/> <img src="sky.jpg" width="320" alt="source_img"/>
+![space](space.jpg)
+![sky](sky.jpg)
 
-Output
+### Output
 
-<img src="output.jpg" width="640" alt="output_img"/>
+![output](output.jpg)
+
+## Method
+
+The transfer is made by sorting each image pixels according to their dot product with a certain vector.
+The two sorted lists of the source and target are then compared to make each pixel of same rank correspond to each 
+other.
+The source can then be rearranged according to these correspondences.
+
+To find the vector that would sort them best, the goal is to rotate a vector in all the positive directions and find the 
+one with the lowest associated cost.
+The search would be done by finding the three angles components of the vector.
+For each x, y and z angle, we start with a unit vector of 0° rotation around the current angle axis, and then compute
+its associated cost.
+Then, we increment the angle by 1°, and repeat the process until 90° at maximum.
+If at some point, the cost becomes higher than the previous one, meaning that we have found an optimal angle, we stop 
+the search and return the previous angle.
+Once each angle is found, we compute the unit vector corresponding to these angles, which is the best vector.
--- color_transfer.py
+++ color_transfer.py
@@ -5,96 +5,134 @@ from PIL import Image
 from tqdm import tqdm
 
 
-def sorted_pixels_from_vector(pixels, vector):
+def cost(pixels_1, pixels_2):
     """
-    Returns the sorted pixels array and the indices that would sort them.\n
-    Sorts all pixels in the pixels array according to their dot products with the vector,
-    ie their projection on the vector.\n
+    Returns the cost between the two pixels arrays.
+
+    :param pixels_1: Pixels array.
+    :param pixels_2: Pixels array.
+    :return: Cost between the sets of pixels.
+    """
+    return np.sum((pixels_1 - pixels_2) ** 2)
+
+
+def sort_by_vector(pixels, vector):
+    """
+    Returns the sorted pixels array and the indices that would sort them.
+    Sorts all pixels in the array according to their dot products with the vector, ie their projection on the
+    vector.
+
     :param pixels: Pixels array to sort.
     :param vector: 3 dimensional vector.
-    :return: The sorted pixels array, and the sorting indices array.
+    :return: Sorted pixels array, sorting indices array.
     """
     sorted_indexes = np.argsort(pixels.dot(vector))
     sorted_pixels = pixels[sorted_indexes]
     return sorted_pixels, sorted_indexes
 
 
-def cost(pixels_1, pixels_2):
+def sort_and_cost(target_pixels, source_pixels, vector):
     """
-    Returns the cost between two arrays of pixels.\n
-    The cost is the sum of all the distances separating each corresponding pixels between the two sets.\n
-    The distance between two pixels is their difference squared.\n
-    :param pixels_1: Pixels array.
-    :param pixels_2: Pixels array.
-    :return: The cost between the two sets.
+    Returns the cost between the target and source that have been sorted by the vector.
+
+    :param target_pixels: Pixels array to sort.
+    :param source_pixels: Pixels array to sort.
+    :param vector: 3 dimensional vector.
+    :return: Cost between the sets of pixels.
     """
-    return np.sum((pixels_1 - pixels_2) ** 2)
+    sorted_target_pixels, _ = sort_by_vector(target_pixels, vector)
+    sorted_source_pixels, _ = sort_by_vector(source_pixels, vector)
+    return cost(sorted_target_pixels, sorted_source_pixels)
 
 
-def random_transport_draw(target_pixels, source_pixels):
+def find_best_vector(target_pixels, source_pixels):
     """
-    Returns the cost of a transport between the two pixels sets, determined randomly by a vector.\n
-    Returns also the sorted source pixels set, with its sorted indexes.\n
-    :param target_pixels: Targeted image pixels array.
-    :param source_pixels: Source image pixels array.
-    :return: Tuple composed of
-    the cost of the transport between the two pixels sets,
-    pixels set sorted according to a random vector,
-    and sorted indices of the the pixels set.
+    Returns the vector that would sort the target and source pixels arrays with the lowest cost.
+
+    :param target_pixels: Pixels array to sort.
+    :param source_pixels: Pixels array to sort.
+    :return: Best vector.
     """
-    vector = np.random.rand(3)
-    sorted_target_pixels, sorted_indexes = sorted_pixels_from_vector(target_pixels, vector)
-    sorted_source_pixels = sorted_pixels_from_vector(source_pixels, vector)[0]
+    best_x_cost = sort_and_cost(target_pixels, source_pixels, np.array((0, 0, 1), dtype=float))
+    best_y_cost = sort_and_cost(target_pixels, source_pixels, np.array((1, 0, 0), dtype=float))
+    best_z_cost = sort_and_cost(target_pixels, source_pixels, np.array((0, 1, 0), dtype=float))
+    best_x_vector = best_y_vector = best_z_vector = np.zeros(3)
 
-    transport_cost = cost(sorted_target_pixels, sorted_source_pixels)
+    # searching the best x-axis angle
+    for angle in tqdm(range(1, 91), desc="Searching x angle"):
 
-    return transport_cost, sorted_source_pixels, sorted_indexes
+        rad = np.deg2rad(angle)
+        vector = np.array((0, np.cos(rad), np.sin(rad)))  # rotating around the x-axis
+        cost_ = sort_and_cost(target_pixels, source_pixels, vector)
+
+        if cost_ < best_x_cost:
+            best_x_cost = cost_
+        else:
+            best_x_vector = vector
+            break
+
+    # searching the best y-axis angle
+    for angle in tqdm(range(1, 91), desc="Searching y angle"):
+
+        rad = np.deg2rad(angle)
+        vector = np.array((np.cos(rad), 0, np.sin(rad)))  # rotating around the y-axis
+        cost_ = sort_and_cost(target_pixels, source_pixels, vector)
+
+        if cost_ < best_y_cost:
+            best_y_cost = cost_
+        else:
+            best_y_vector = vector
+            break
+
+    # finding the best z-axis angle
+    for angle in tqdm(range(1, 91), desc="Searching z angle"):
+
+        rad = np.deg2rad(angle)
+        vector = np.array((np.cos(rad), np.sin(rad), 0))  # rotating around the z-axis
+        cost_ = sort_and_cost(target_pixels, source_pixels, vector)
+
+        if cost_ < best_z_cost:
+            best_z_cost = cost_
+        else:
+            best_z_vector = vector
+            break
+
+    best_vector = best_y_vector + best_x_vector + best_z_vector
+    best_vector /= np.linalg.norm(best_vector)  # converting to unit vector
+
+    print(f"Best sorting vector: {np.round(best_vector, 2)}")
+    return best_vector
 
 
-def color_transfer(target_pixels, source_pixels, n):
+def color_transfer(target_pixels, source_pixels):
     """
-    Returns the resulting pixels array of a color transfer from the source pixels array to the target pixels array.\n
+    Returns the resulting pixels array of a color transfer from the source pixels array to the target pixels array.
+
     :param target_pixels: Targeted pixels array, whose colors must change.
     :param source_pixels: Source pixels array, used as a palette.
-    :param n: Number of random transport to try.
     :return: Output pixels array.
     """
-
-    print("initialization")
-
     # the best vector is the vector with the lowest associated cost,
     # we initialize it randomly with its associated pixels set
-    best_cost, best_sorted_pixels, best_sorted_indexes = \
-        random_transport_draw(target_pixels, source_pixels)
+    best_vector = find_best_vector(target_pixels, source_pixels)
 
-    for _ in tqdm(range(n), desc="processing"):
+    _, sorting_indexes = sort_by_vector(target_pixels, best_vector)
+    sorted_source_pixels, _ = sort_by_vector(source_pixels, best_vector)
 
-        current_cost, current_sorted_pixels, current_sorted_indexes = \
-            random_transport_draw(target_pixels, source_pixels)
-
-        if current_cost < best_cost:
-            best_cost = current_cost
-            best_sorted_pixels = current_sorted_pixels
-            best_sorted_indexes = current_sorted_indexes
-
-    # reorganization of each pixels according to their corresponding pixel on target image
-    output_pixels = np.zeros(best_sorted_pixels.shape).astype(int)
-    output_pixels[best_sorted_indexes] = best_sorted_pixels
-
-    print("color transfer finished")
+    # reorganization of each pixel according to their corresponding pixel on target image
+    output_pixels = np.zeros(sorted_source_pixels.shape).astype(int)
+    output_pixels[sorting_indexes] = sorted_source_pixels
 
     return output_pixels
 
 
-def main(target_file_name, source_file_name, output_file_name, n, show_output):
+def main(target_file_name, source_file_name, output_file_name, show_output):
     """
     :param target_file_name: Target image file path.
     :param source_file_name: Source image file path.
     :param output_file_name: Output image file path.
-    :param n: Number of transport draw to do.
     :param show_output: Shows preview of the output image at the end if true.
     """
-
     # loading images
     target_im = Image.open(target_file_name).convert("RGB")
     source_im = Image.open(source_file_name).convert("RGB")
@@ -104,11 +142,12 @@ def main(target_file_name, source_file_name, output_file_name, n, show_output):
         exit("The two images must have the same number of pixels.")
 
     # images conversion into pixels array
+    print("Converting images into arrays...")
     target_pixels = np.array(target_im.getdata())
     source_pixels = np.array(source_im.getdata())
 
     # main program
-    output_pixels = color_transfer(target_pixels, source_pixels, n)
+    output_pixels = color_transfer(target_pixels, source_pixels)
 
     # converting pixels output to a new image
     output_pixels = np.reshape(output_pixels, (target_im.size[1], target_im.size[0], 3))
@@ -116,7 +155,7 @@ def main(target_file_name, source_file_name, output_file_name, n, show_output):
 
     # saving
     output_im.save(output_file_name)
-    print("output saved at", output_file_name)
+    print(f"Output saved in `{output_file_name}`")
 
     if show_output:
         output_im.show()
@@ -128,8 +167,7 @@ if __name__ == "__main__":
     parser.add_argument("target_image", type=str, help="Targeted image.")
     parser.add_argument("source_image", type=str, help="Source image.")
     parser.add_argument("-sa", "--save", type=str, default="output.jpg", help="Output file to save.")
-    parser.add_argument("-n", "--transport-draws", type=int, default=20, help="Number of transports to try.")
-    parser.add_argument("-sh", "--show", action="store_true", help="True to preview output at the end.")
+    parser.add_argument("-sh", "--show", action="store_true", help="Preview the output at the end.")
     args = parser.parse_args()
 
-    main(args.target_image, args.source_image, args.save, args.transport_draws, args.show)
+    main(args.target_image, args.source_image, args.save, args.show)
